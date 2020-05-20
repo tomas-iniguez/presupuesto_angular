dc4e98907b046920cdf699144ccb5d019d876f85
Tomas Iñiguez <tomas.iniguez.gonzalez@gmail.com>
2020-05-20 00:32:34 -0400

    Servicio get que obtiene los planes y inmuebles

--- api/app/Http/Controllers/PresupuestoController.php
+++ api/app/Http/Controllers/PresupuestoController.php
@@ -1,5 +1,10 @@
 <?php
 
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
+
+
 namespace App\Http\Controllers;
 
 class PresupuestoController extends Controller
@@ -13,6 +18,23 @@ class PresupuestoController extends Controller
     {
         //
     }
+    public function componentes(){
+        $arrPlan = [];
+        $arrInmueble = [];
 
-    //
+        $tipoPlanes =  \App\TipoPlan::all();
+        foreach($tipoPlanes as $plan){
+            $arrPlan [] = array('id' => $plan->id, 'nombre' => $plan->nombre);
+        }
+
+        $tipoInmuebles =  \App\TipoInmueble::all();
+        foreach($tipoInmuebles  as $inmueble){
+            $arrInmueble [] = array('id' => $inmueble->id, 'nombre' => $inmueble->nombre);
+        }
+        
+        if(count($arrPlan) > 0 && count($arrInmueble) > 0 ){
+            return response()->json(['estado' => true ,'tipoPlan' => $arrPlan, 'tipoInmueble' => $arrInmueble ], 200);
+        }
+        return response()->json([ 'estado' => false, 'mensaje' => 'Sin datos disponible' ], 404);
+    }
 }
---
+++ api/app/TipoPlan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipoPlan extends Model
+{
+    
+    protected $table = 'tipo_plan';
+}
